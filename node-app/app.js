66a853a7930dc0f4bc3c525a09f65d08840d8464
Shusuke Nakamura <shusuke.nakamura@systemi.com>
2023-07-19 16:56:40 +0900

    リスト3-3まで学習完了

--- node-app/app.js
+++ node-app/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -16,27 +17,10 @@ function getFromClient(request, response) {
     var url_parts = url.parse(request.url, true);
     switch(url_parts.pathname) {
         case '/':
-            var content = "これはIndexページです。";
-            var query = url_parts.query;
-            if (query.msg != undefined) {
-                content += 'あなたは、「' + query.msg + '」と送りました。';
-            }
-            var content = ejs.render(index_page,{
-                title: 'Indexページ',
-                content: content,
-            });
-            response.writeHead(200, {'Content-Type' : 'text/html'});
-            response.write(content);
-            response.end();
+            response_index(request, response);
             break;
         case '/other':
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: "これは新しく用意したページです。",
-            });
-            response.writeHead(200, {"content-type" : "text/html"});
-            response.write(content);
-            response.end();
+            response_other(request, response);
             break;
         case '/style.css':
             response.writeHead(200, {'Content-Type' : 'text/css'});
@@ -47,5 +31,48 @@ function getFromClient(request, response) {
             response.writeHead(200, {'Content-Type' : 'text/plain'});
             response.end('no page...');
             break;
+    }
+}
+
+function response_index(request, response) {
+    var msg = "これはIndexページです。";
+    var content = ejs.render(index_page, {
+        title: "Index",
+        content: msg,
+    });
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write(content);
+    response.end();
+}
+
+function response_other(request, response) {
+    var msg = "これはOtherページです";
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        request.on('end', () => {
+            var post_data = qs.parse(body);
+            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
+            var content = ejs.render(other_page, {
+                title: "Other",
+                content: msg,
+            })
+            response.writeHead(200, {"Content-Type" : "text/html"});
+            response.write(content);
+            response.end();
+        });
+    } else {
+        var msg = "ページがありません。";
+        var content = ejs.render(other_page, {
+            title: "Other",
+            content: msg,
+        })
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        response.write(content);
+        response.end();
     }
 }
