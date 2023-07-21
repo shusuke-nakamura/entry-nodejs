a54aa2b1348aa3ac86890c69c2cbd1b121850820
Shusuke Nakamura <shusuke.nakamura@systemi.com>
2023-07-21 16:24:10 +0900

    リスト5-13まで学習完了

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -18,10 +18,101 @@ router.get('/', (req, res,next) => {
                     title: 'Hello',
                     content: rows,
                 };
-                res.render('hello', data);
+                res.render('hello/index', data);
             }
         });
     });
+});
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/Add',
+        content: '新しいレコードを入力:'
+    }
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+
+    db.serialize(() => {
+        db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    });
+    res.redirect('/hello');
+});
+
+router.get('/show', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/show',
+                    content: 'id = ' + id + 'のレコード:',
+                    mydata: row,
+                }
+                res.render('hello/show', data);
+            }
+        });
+    });
+});
+
+router.get('/edit', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/edit',
+                    content: 'id = ' + id + ' のレコードを編集:',
+                    mydata: row,
+                };
+                res.render('hello/edit', data);
+            }
+        });
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+    const id = req.body.id;
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    const q = 'update mydata set name = ?, mail = ?, age = ? where id = ?';
+    db.serialize(() => {
+        db.run(q, nm, ml, ag, id);
+    });
+    res.redirect('/hello');
+});
+
+router.get('/delete', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Delete',
+                    content: 'id = ' + id + ' のレコードを削除:',
+                    mydata: row,
+                };
+                res.render('hello/delete', data);
+            }
+        });
+    });
+});
+
+router.post('/delete', (req, res, next) => {
+    const id = req.body.id;
+    db.serialize(() => {
+        const q = 'delete from mydata where id = ?';
+        db.run(q, id);
+    });
+    res.redirect('/hello');
 });
 
 module.exports = router;
